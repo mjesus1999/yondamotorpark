15275430c44c1e32ef8227a2b5fed92faf72ea37
Yamilet Arias <deya.meme.08@gmail.com>
2025-10-23 17:51:42 -0500

    SE MODIFICO COTIZACIONES Y CREDITO / LOGIN ESTETICA

---
+++ app/Views/auth/loginPrueba.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOTORPARK </title>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700;800&family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+</head>
+<body>
+
+    <!-- CUADRO DEL LOGIN -->
+    <div class="login-container">
+        <!-- IMAGEN -->
+        <div class="image-section">
+            <div class="brand-section">
+                <h1 class="logo-title">Motorpoark</h1>
+                <p class="logo-subtitle">Yonda</p>
+            </div>
+
+            <!-- <div class="features">
+                <div class="feature">
+                    <i class="fas fa-car"></i>
+                    <div class="feature-text">
+                        <h4>Autos Premium</h4>
+                        <p>Las mejores marcas del mercado</p>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+
+        <!-- FORMULARIO -->
+    </div>
+    
+</body>
+</html>
